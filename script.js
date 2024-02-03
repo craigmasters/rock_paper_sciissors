fd3ab39824ae61f2c3d36febaaf448bd45f436a1
Craig Masters <44289909+craigmasters@users.noreply.github.com>
2024-02-03 16:41:38 +0000

    mvp complete

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
+let playerScore = 0;
+let computerScore = 0;
+
 // Get input from the player (Must be case-insensitive)
 function playerSelection(playerChoice) {
     playerChoice = prompt("Rock, Paper or Scissors?", "");
     playerChoice = playerChoice.toLowerCase();
 
     if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-        // console.log(playerChoice);
         return playerChoice;
     }
     else {
         playerSelection();
     }
 }
-    let playerScore = 0;
-    let computerScore = 0;
-
 
 // Get randomly selected option for computerChoice
 function getComputerChoice(computerChoice) {
@@ -22,63 +21,76 @@ function getComputerChoice(computerChoice) {
 }
 
 
+
 function playGame() {
-    let player = playerSelection();
-    let computer = getComputerChoice();
-    let playerScore = 0;
-    let computerScore = 0;
 
-    if (playerScore <= 2 || computerScore <= 2) {
-        playRound();
-    }
-    else if (playerScore === 3) {
-        return (console.log("You WON the first to 3!"))
-    }
-    else if (computerScore === 3) {
-        return (console.log("You LOST the first to 3!"))
-    }
+    let rounds = 1;
+    playRound();
 
-
-// Function for playRound
+    // Function for playRound
     function playRound() {
-        if (player === "rock" && computer === "scissors") {
-            playerScore = playerScore +1;
-            console.log("Player: " + player + " - Computer: " + computer);
-            console.log("Rock beats Scissors, you win this round!");
-            console.log(playerScore);
-            console.log(computerScore);
-        }
-        else if (player === "paper" && computer === "rock") {
-            playerScore = playerScore +1;
-            console.log("Player: " + player + " - Computer: " + computer);
-            console.log("Paper beats Rock, you win this round!");
-            console.log(playerScore);
-            console.log(computerScore);
-        }
-        else if (player === "scissors" && computer === "paper") {
-            playerScore = playerScore +1;
-            console.log("Player: " + player + " - Computer: " + computer);
-            console.log("Scissors beats Paper, you win this round!");
-            console.log(playerScore);
-            console.log(computerScore);
-        }
-        else if (player === computer) {
-            console.log("Player: " + player + " - Computer: " + computer);
-            console.log("Its a draw!");
-            console.log(playerScore);
-            console.log(computerScore);
-        }
-        else {
-            computerScore = computerScore +1;
-            console.log("Player: " + player + " - Computer: " + computer);
-            console.log("You lose this round!");
-            console.log(playerScore);
-            console.log(computerScore);
+        while (rounds <=5) {
+
+            let player = playerSelection();
+            let computer = getComputerChoice();
+            console.log(rounds);
+            rounds = rounds +1;
+
+            if (player === "rock" && computer === "scissors") {
+                playerScore = playerScore +1;
+                console.log("Player: " + player + " - Computer: " + computer);
+                console.log("Rock beats Scissors, you win this round!");
+                console.log("Player score: " + playerScore);
+                console.log("Computer score: " + computerScore);
+            }
+
+            else if (player === "paper" && computer === "rock") {
+                playerScore = playerScore +1;
+                console.log("Player: " + player + " - Computer: " + computer);
+                console.log("Paper beats Rock, you win this round!");
+                console.log("Player score: " + playerScore);
+                console.log("Computer score: " + computerScore);
+            }
+
+            else if (player === "scissors" && computer === "paper") {
+                playerScore = playerScore +1;
+                console.log("Player: " + player + " - Computer: " + computer);
+                console.log("Scissors beats Paper, you win this round!");
+                console.log("Player score: " + playerScore);
+                console.log("Computer score: " + computerScore);
+            }
+
+            else if (player === computer) {
+                console.log("Player: " + player + " - Computer: " + computer);
+                console.log("Its a draw!");
+                console.log("Player score: " + playerScore);
+                console.log("Computer score: " + computerScore);
+            }
+
+            else {
+                computerScore = computerScore +1;
+                console.log("Player: " + player + " - Computer: " + computer);
+                console.log("You lose this round!");
+                console.log("Player score: " + playerScore);
+                console.log("Computer score: " + computerScore);
+            }
         }
     }
 }
 
+function scoring(){
+    if (playerScore > computerScore) {
+        console.log("After 5 rounds - You beat the computer!")
+    }
+    else if (computerScore > playerScore) {
+        console.log("After 5 rounds - The computer beat you!")
+    }
+    else {console.log("After 5 rounds - Its a draw!")};
+}
+
+
 playGame();
+scoring();
 
 
 
